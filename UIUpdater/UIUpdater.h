fdf4d4e47b9abc9c1978f3433737a10e1301fc85
Matt Hogg <matt.hogg74@hotmail.com>
2025-03-04 10:19:28 +0000

    Initial Commit

---
+++ UIUpdater/UIUpdater.h
@@ -0,0 +1,18 @@
+//
+//  UIUpdater.h
+//  UIUpdater
+//
+//  Created by Matt Hogg on 04/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIUpdater.
+FOUNDATION_EXPORT double UIUpdaterVersionNumber;
+
+//! Project version string for UIUpdater.
+FOUNDATION_EXPORT const unsigned char UIUpdaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIUpdater/PublicHeader.h>
+
+
